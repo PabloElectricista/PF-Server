4694f61bcb449de751107fc443f580a2e425e2b6
TheSerch07 <jejog50@gmail.com>
2022-09-14 15:03:27 -0500

    user db

--- src/models/User.js
+++ src/models/User.js
@@ -40,6 +40,7 @@ const productSchema = new mongoose.Schema(
   }
 );
 
+
 productSchema.statics.encryptPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
