13f997627a6dd63616cbdfea3c30fa78c04961de
Roger Miño <roger.rm03@gmail.com>
2022-09-14 01:08:19 -0300

    fake data cargada

--- src/controllers/filter.controller.js
+++ src/controllers/filter.controller.js
@@ -4,18 +4,14 @@ export const filterProduct= async (req,res)=>{
     try{
         const condition={}; //condicion de busquerda
         for(const prop in req.query){
-            if(prop=="name"){
-
-                condition[prop]=new RegExp(req.query[prop],"i")
-            }
-            else if(prop=="price"){
+            if(prop=="price"){
                 let [min,max]=req.query[prop].split('/') //price?min/max desde el front
                 condition[prop]={
                     $lte:parseInt(max),
                     $gte:parseInt(min)
                 }
             }else
-                condition[prop]=req.query[prop]
+                condition[prop]=new RegExp(req.query[prop],"i")
         }
         const foundProducts=await Products.find(condition)
         res.status(201).send(foundProducts)
