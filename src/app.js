dc0549f2d95ab4876be7d4142873d9199aac5141
TheSerch07 <jejog50@gmail.com>
2022-09-23 13:52:37 -0500

    cloudinay back

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
+import fileUpload from "express-fileupload"
 import initialfunction from "./libs/initialfunction.js"
 // Routes
 import indexRoutes from "./routes/index.routes.js";
@@ -24,6 +25,10 @@ app.use(
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: "./uploads"
+}));
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
