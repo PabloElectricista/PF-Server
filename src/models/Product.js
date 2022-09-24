463b22eddfcfba544d1b2cf90699dbaa9fb7f08c
Pablo Spector <pablo_spector@hotmail.com>
2022-09-23 21:41:08 -0300

    arreglando el lio de Sergio

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,26 +1,5 @@
 import { model, Schema } from "mongoose";
 
-const reviewSchema = new mongoose.Schema(
-    {
-        name: { 
-            type: String, 
-            required: true 
-        },
-        comment: { 
-            type: String, 
-            required: true 
-        },
-        rating: { 
-            type: Number, 
-            required: true 
-        },
-    },
-    {
-        timestamps: true,
-    }
-);
-    
-
 const productSchema = new Schema(
     {
         name: {
